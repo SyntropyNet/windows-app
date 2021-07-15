c8f4f592ec78cf7c6c767d97772d8a0a7eb95d25
Kirill <shalam.kirill@gmail.com>
2021-07-15 14:23:26 +0700

    Fix routing table size when get info

--- SyntropyNet.WindowsApp.Application/Services/NetworkInformation/NativeMethods.cs
+++ SyntropyNet.WindowsApp.Application/Services/NetworkInformation/NativeMethods.cs
@@ -18,7 +18,7 @@ namespace SyntropyNet.WindowsApp.Application.Services.NetworkInformation
         {
             public uint Size;
 
-            [MarshalAs(UnmanagedType.ByValArray)]
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 30)]
             public MIB_IPFORWARDROW[] Table;
         };
 
