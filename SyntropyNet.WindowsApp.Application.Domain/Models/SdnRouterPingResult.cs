f76c0e7f369c2116f8f6c4ff42e136544b243ffa
Kirill <shalam.kirill@gmail.com>
2021-10-06 14:40:43 +0700

    Change ping intervals to 30s with statistics collection

---
+++ SyntropyNet.WindowsApp.Application.Domain/Models/SdnRouterPingResult.cs
@@ -0,0 +1,33 @@
+﻿using SyntropyNet.WindowsApp.Application.Domain.Enums.WireGuard;
+
+namespace SyntropyNet.WindowsApp.Application.Domain.Models {
+    public class SdnRouterPingResult {
+        private int _hashCode = 0;
+
+        public long Latency { get; set; }
+        public string Ip { get; set; }
+        public string Peer { get; set; }
+        public string Gateway { get; set; }
+        public int ConnectionId { get; set; }
+        public WGInterfaceName InterfaceName { get; set; }
+
+        public int Hash {
+            get {
+                if (_hashCode == 0) {
+                    string[] keyParts = new string[] {
+                        Ip,
+                        Peer,
+                        Gateway,
+                        InterfaceName.ToString(),
+                        ConnectionId.ToString()
+                    };
+
+                    string keyString = string.Join("", keyParts);
+                    _hashCode = keyString.GetHashCode();
+                }
+
+                return _hashCode;
+            }
+        }
+    }
+}
